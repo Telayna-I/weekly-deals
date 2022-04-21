0e93f1635461772f6b24a449d651594811dc4ef5
Telayna-I <telayna98@gmail.com>
2022-04-21 01:10:52 -0300

    Instaladas las dependecias que creo son necesarias para empezar

---
+++ src/Services/Firebase/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDbWpCZKqDSJFUyfgHYG15CbTiAYoX7OFA",
+    authDomain: "weekly-deals-c1cbc.firebaseapp.com",
+    projectId: "weekly-deals-c1cbc",
+    storageBucket: "weekly-deals-c1cbc.appspot.com",
+    messagingSenderId: "564804787005",
+    appId: "1:564804787005:web:a0c4cdd8ddaf9478860354"
+};
+
+const app = initializeApp(firebaseConfig);
